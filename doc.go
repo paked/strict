409d2bde784041f5b01a8cff55ad0b6a13dfa55a
Harrison <harrison@theshoebridges.com>
2015-03-28 20:54:22 +1100

    doc: add language samples

--- doc.go
+++ doc.go
@@ -15,3 +15,47 @@
 // <expression> := <variable> (<operator> <variable>)?
 // <variable> := <variable_name> | <number> | <string>
 package strict
+
+/*
+{
+	string_variable = "Hello"
+	integer_variable = 2
+
+	// output: Hello 2
+	println(string_variable, integer_variable)
+
+
+	i = number()
+
+	// output: 2
+	println(i)
+
+	c = [2, 3] >> add()
+
+	// output: 5
+	println(c)
+
+	z = {
+		// computations and stuff
+		return 10
+	}()
+
+	// output: 10
+	println(z)
+
+	// output: hello
+	hello()
+}
+
+number {
+	return 2
+}
+
+add [int a, int b] {
+	return a + b
+}
+
+hello {
+	println("hello")
+}
+*/
