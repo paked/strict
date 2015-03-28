1066c251a4ba7cacd9336dc64e84231a3c76efe3
Harrison <harrison@theshoebridges.com>
2015-03-28 22:49:35 +1100

    lexer: write lexing functions for arrays

--- lexer.go
+++ lexer.go
@@ -1,6 +1,7 @@
 package strict
 
 import (
+	"fmt"
 	"unicode"
 )
 
@@ -9,12 +10,21 @@ type Token struct {
 	Value string
 }
 
+func (t Token) String() string {
+	var postfix string
+	if len(t.Value) > 1 {
+		postfix = ":" + t.Value
+	}
+	return t.Type + postfix
+}
+
 type Lexer struct {
 	source   string
 	location int
 }
 
-func (lex *Lexer) Lex() ([]Token, err) {
+func (lex *Lexer) Lex() ([]Token, error) {
+	fmt.Println(lex.source)
 	tokens := []Token{}
 
 	for !lex.End() {
@@ -23,6 +33,10 @@ func (lex *Lexer) Lex() ([]Token, err) {
 			tokens = append(tokens, Token{"SCOPE_START", "{"})
 		} else if c == '}' {
 			tokens = append(tokens, Token{"SCOPE_END", "}"})
+		} else if toks, ok := lex.list(); ok {
+			for _, token := range toks {
+				tokens = append(tokens, token)
+			}
 		}
 
 		lex.Next()
@@ -31,35 +45,125 @@ func (lex *Lexer) Lex() ([]Token, err) {
 	return tokens, nil
 }
 
-func (lex *Lexer) statement() ([]Token, bool) {
-	start := lex.location
-	ok, name := lex.variableName()
-	if !ok {
-		return false, name
+func (lex *Lexer) space() {
+	for !lex.End() {
+		if c := lex.Peek(); !unicode.IsSpace(c) {
+			break
+		}
+		fmt.Println("char:", string(lex.Peek()))
+		lex.Next()
 	}
-
 }
 
-func (lex *Lexer) variableName() (Token, bool) {
-	start := lex.location
-	var name string
+func (lex *Lexer) list() ([]Token, bool) {
+	old := lex.location
+
+	tokens := []Token{}
+	start, ok := lex.listStart()
+	if !ok {
+		fmt.Println("no list start", string(lex.Peek()))
+		return tokens, false
+	}
+
+	tokens = append(tokens, start)
 
 	for !lex.End() {
+		value, ok := lex.string()
+		if !ok {
+			fmt.Println("no value")
+			return tokens, false
+			// no more list values -- something went wrong...
+		}
+		tokens = append(tokens, value)
+		lex.space()
 		c := lex.Peek()
-		if unicode.IsDigit(c) || unicode.IsNumber(c) {
-			name += c
+
+		fmt.Println("char:", string(lex.Peek()))
+		if c != ',' {
+			end, ok := lex.listEnd()
+			if !ok {
+				lex.location = old
+				return tokens, false
+			}
+
+			tokens = append(tokens, end)
+			return tokens, true
+		}
+		lex.Next()
+		lex.space()
+	}
+
+	return tokens, false
+}
+
+func (lex *Lexer) listStart() (Token, bool) {
+	if lex.Peek() != '[' {
+		return Token{}, false
+	}
+
+	fmt.Println("STARTING ARRAY")
+	lex.Next()
+
+	return Token{"LIST_START", "["}, true
+}
+
+func (lex *Lexer) listValue() (Token, bool) {
+	var content string
+	for !lex.End() {
+		c := lex.Peek()
+		fmt.Println(string(c))
+		if unicode.IsDigit(c) || unicode.IsLetter(c) {
+			content += string(c)
 			lex.Next()
 			continue
 		}
 
+		ok := true
+		if content == "" {
+			fmt.Println("EMPTY CONTENT")
+			ok = false
+		}
+
+		return Token{"LIST_VALUE", content}, ok
+	}
+
+	return Token{}, false
+}
+
+func (lex *Lexer) string() (Token, bool) {
+	if c := lex.Peek(); c != '"' {
 		return Token{}, false
 	}
 
-	return Token{"VARIABLE_NAME", name}, true
+	lex.Next()
+	var content string
+	for !lex.End() {
+		c := lex.Peek()
+		if c == '"' {
+			lex.Next()
+			fmt.Println(content)
+			return Token{"STRING", content}, true
+		}
+
+		content += string(c)
+		lex.Next()
+	}
+
+	return Token{}, false
+}
+
+func (lex *Lexer) listEnd() (Token, bool) {
+	if lex.Peek() != ']' {
+		return Token{}, false
+	}
+
+	lex.Next()
+
+	return Token{"LIST_END", "]"}, true
 }
 
 func (lex *Lexer) End() bool {
-	if lex.location > len(lex.source) {
+	if lex.location >= len(lex.source) {
 		return true
 	}
 
