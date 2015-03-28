512ef1cb142020c2317016d2f94e1e318c351a0c
Harrison <harrison@theshoebridges.com>
2015-03-28 21:37:05 +1100

    doc: rewrote grammar

--- doc.go
+++ doc.go
@@ -17,6 +17,42 @@
 package strict
 
 /*
+	<program> := <scope>+
+	<scope> := <scope_name>? | "{" <line> "}"
+	<line> := <statement> | <control>
+
+	<evaluate_scope> := <args>? <scope> "()"
+	<args> := <list> ">>"
+	<list> := "[" <list_value> "]"
+	<list_value> := <value> ("," <list_value>)
+	<int> := / \d* /
+	<float> := / \d*.\d* /
+	<number> := <int> | <float>
+	<string> := "\"" / .* / "\""
+	<variable_name> := / [a-z][a-zA-Z0-9]*  /
+	<value> := <number> | <string> | <evaluate_scope> | <variable_name>
+
+	<add> := "+"
+	<subtract> := "-"
+	<divide> := "/"
+	<multiply> := "*"
+	<operator> := <add> | <subtract> | <divide> | <multiply>
+
+	<or> := "or"
+	<and> := "and"
+	<more> := <or> | <and>
+
+	<statement> := <assignment>? <expression>
+	<assignment> := <variable_name> "="
+	<expression> := <value> (<operator> <value>)? (<more> <expression>)?
+
+	<while> := "while" <expression> <scope>
+	<if> := "if" <expression> <scope>
+	<control> := <while> | <if>
+
+	<scope_name> := /[A-Z][a-zA-Z0-9]+/
+*/
+/*
 {
 	string_variable = "Hello"
 	integer_variable = 2
@@ -25,17 +61,17 @@ package strict
 	println(string_variable, integer_variable)
 
 
-	i = number()
+	int i = number()
 
 	// output: 2
 	println(i)
 
-	c = [2, 3] >> add()
+	int c = [2, 3] >> add()
 
 	// output: 5
 	println(c)
 
-	z = {
+	int z = {
 		// computations and stuff
 		return 10
 	}()
@@ -45,13 +81,30 @@ package strict
 
 	// output: hello
 	hello()
+
+	int d = 0
+	int e = 3
+
+	// output: hi hi hi
+	while d < e {
+		println("hi")
+		d = d + 1
+	}
+
+	int x = 2
+
+	// output: eh eh
+	while x < 2 and true {
+		println("eh")
+		x = x + 1
+	}
 }
 
 number {
 	return 2
 }
 
-add [int a, int b] {
+[int a, int b] >> add{
 	return a + b
 }
 
